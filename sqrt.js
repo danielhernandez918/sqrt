8ab66d80d4ade261e5bd71d94ad06c4f156c5b4d
Daniel Hernandez <danielhernandez918@gmail.com>
2022-04-07 23:04:38 -0700

    first commit

---
+++ sqrt.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+const x1 = 4
+
+const x2 = 8
+var mySqrt = function(x) {
+    const number = Math.floor(Math.sqrt(x));
+    return number
+};
+
+console.log(mySqrt(x1));
+console.log(mySqrt(x2));
